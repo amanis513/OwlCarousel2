c76dc5e0e35dc1c4fe1b21877a5c0da915b573a5
Jozef Dubecky <dubecky@gmail.com>
2016-09-29 20:28:40 +0200

    update

--- dist/owl.carousel.js
+++ dist/owl.carousel.js
@@ -1,11 +1,11 @@
 /**
- * Owl Carousel v2.1.6
+ * Owl Carousel v2.1.4
  * Copyright 2013-2016 David Deutsch
  * Licensed under MIT (https://github.com/OwlCarousel2/OwlCarousel2/blob/master/LICENSE)
  */
 /**
  * Owl carousel
- * @version 2.1.6
+ * @version 2.1.0
  * @author Bartosz Wojciechowski
  * @author David Deutsch
  * @license The MIT License (MIT)
@@ -24,7 +24,7 @@
 	 * @param {HTMLElement|jQuery} element - The element to create the carousel for.
 	 * @param {Object} [options] - The options
 	 */
-	function Owl(element, options) {
+	function Owl(element,options) {
 
 		/**
 		 * Current settings for the carousel.
@@ -515,9 +515,6 @@
 			});
 
 			settings = $.extend({}, this.options, overwrites[match]);
-			if (typeof settings.stagePadding === 'function') {
-				settings.stagePadding = settings.stagePadding();
-			}
 			delete settings.responsive;
 
 			// responsive class
@@ -528,11 +525,13 @@
 			}
 		}
 
-		this.trigger('change', { property: { name: 'settings', value: settings } });
-		this._breakpoint = match;
-		this.settings = settings;
-		this.invalidate('settings');
-		this.trigger('changed', { property: { name: 'settings', value: this.settings } });
+		if (this.settings === null || this._breakpoint !== match) {
+			this.trigger('change', { property: { name: 'settings', value: settings } });
+			this._breakpoint = match;
+			this.settings = settings;
+			this.invalidate('settings');
+			this.trigger('changed', { property: { name: 'settings', value: this.settings } });
+		}
 	};
 
 	/**
@@ -1023,23 +1022,17 @@
 	Owl.prototype.maximum = function(relative) {
 		var settings = this.settings,
 			maximum = this._coordinates.length,
-			iterator,
-			reciprocalItemsWidth,
-			elementWidth;
+			boundary = Math.abs(this._coordinates[maximum - 1]) - this._width,
+			i = -1, j;
 
 		if (settings.loop) {
 			maximum = this._clones.length / 2 + this._items.length - 1;
 		} else if (settings.autoWidth || settings.merge) {
-			iterator = this._items.length;
-			reciprocalItemsWidth = this._items[--iterator].width();
-			elementWidth = this.$element.width();
-			while (iterator--) {
-				reciprocalItemsWidth += this._items[iterator].width() + this.settings.margin;
-				if (reciprocalItemsWidth > elementWidth) {
-					break;
-				}
+			// binary search
+			while (maximum - i > 1) {
+				Math.abs(this._coordinates[j = maximum + i >> 1]) < boundary
+					? i = j : maximum = j;
 			}
-			maximum = iterator + 1;
 		} else if (settings.center) {
 			maximum = this._items.length - 1;
 		} else {
